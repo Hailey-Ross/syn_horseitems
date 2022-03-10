28ea494bf306f4977579d59d5fa900b862ed0d40
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-09 17:30:18 -0700

    Update runme_horseitems.sql

--- runme_horseitems.sql
+++ runme_horseitems.sql
@@ -1,4 +1,7 @@
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_haycube', 'Haycube', 25, 1, 'item_standard', 0);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('carrot', 'Carrot', 12, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugarcube', 'Sugar Cube', 10, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('apple', 'Apple', 15, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_peach', 'Peach', 15, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('horsebrush', 'Horsebrush', 25, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('stim', 'Horse Stimulant', 5, 1, 'item_standard', 1);
