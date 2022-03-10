9aacf6877818cad68352545839aa6f1129a63cb7
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-10 09:44:27 -0700

    Update runme_horseitems.sql

--- runme_horseitems.sql
+++ runme_horseitems.sql
@@ -1,5 +1,6 @@
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_haycube', 'Haycube', 25, 1, 'item_standard', 0);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('carrot', 'Carrot', 12, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Wild_Carrot', 'Wild Carrot', 12, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugarcube', 'Sugar Cube', 10, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('apple', 'Apple', 15, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_peach', 'Peach', 15, 1, 'item_standard', 1);
